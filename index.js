457ed071f219fb2aa2ad7ec415044491a3f7c575
Navaneeth223 <navaneethkv1002@gmail.com>
2025-09-15 21:10:56 +0530

    app.js

---
+++ index.js
@@ -0,0 +1,19 @@
+// var a = 10;
+// console.log(a);
+// var a=true;
+// console.log(a);
+
+// let a = 10;
+// console.log(a);
+// let a = true;
+// console.log(a);
+
+// const a = 10;
+// console.log(a);
+// const a = true;
+// console.log(a);
+
+
+console.log(a);
+var a = 10;
+console.log(a);      
