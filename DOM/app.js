7fc7511a745b40441f8518f4d68dc18416b8ce50
Navaneeth223 <navaneethkv1002@gmail.com>
2025-10-06 19:34:48 +0530

    new

--- DOM/app.js
+++ DOM/app.js
@@ -1,1 +1,20 @@
-let h1 = document.getElementsByTagName("h1");
+// let h1 = document.querySelector("h1");
+
+
+// // wet
+
+// h1.style.color = "red";
+// h1.style.backgroundColor = "black";
+// h1.style.textAlign = "center";
+
+// // manipulating multiple styles is
+// dry
+
+let h1 = document.querySelector("h1");
+
+
+h1.style.cssText = `
+    color: red;
+    background-color: blue;
+    font-size: 50px;
+`;  
---
+++ DOM/text_manipulation/app.js
@@ -0,0 +1,6 @@
+let p = document.querySelector("p");
+
+
+
+p.innerText = "Hello World";
+p.innerHTML = "Hello <b>World</b>";
