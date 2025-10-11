252e5667fa97e52aa45bac19c27f002aa3107909
Navaneeth223 <navaneethkv1002@gmail.com>
2025-10-11 17:33:45 +0530

    sdghssfd
    sdfsc

---
+++ DOM/dom_tree_traversal/index.js
@@ -0,0 +1,3 @@
+let para = document.querySelector("para");
+
+console.log(para.children[1].children.style.color = "red"); // NodeList(3) [text, span, text]
--- DOM/text_manipulation/app.js
+++ DOM/text_manipulation/app.js
@@ -4,3 +4,12 @@ let p = document.querySelector("p");
 
 p.innerText = "Hello World";
 p.innerHTML = "Hello <b>World</b>";
+
+let
+
+let images = [
+    'https://images.unsplash.com/photo-1759675739458-6e5a4a60a117?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8',
+    'https://plus.unsplash.com/premium_photo-1701062876279-f17c71ec7f98?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8',
+    'https://plus.unsplash.com/premium_photo-1755001060420-7a561e278353?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3fHx8ZW58MHx8fHx8',
+    'https://images.unsplash.com/photo-1759681770982-313332e7f42c?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxN3x8fGVufDB8fHx8fA%3D%3D'
+]
