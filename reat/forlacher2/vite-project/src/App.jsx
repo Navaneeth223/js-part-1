13b029689962637afd082e5f28d71afcb9b3bb6c
Navaneeth223 <navaneethkv1002@gmail.com>
2025-12-14 21:04:34 +0530

    q

--- reat/forlacher2/vite-project/src/App.jsx
+++ reat/forlacher2/vite-project/src/App.jsx
@@ -8,3 +8,16 @@ function App() {
 }
 
 export default App
+
+Build an EmployeeCard component in React to display name, designation and work experience of a person. Pass name, designation and work experience as props.
+
+The colour of “Designation:” should be green.
+The colour of “Experience:” should be blue.
+
+
+----- 2nd question below -------
+Build a React component to display a list of stationery items with a header. The items and header should be passed as props. Header should be “Stationery Items”.
+
+Data:
+
+const items = ['pen', 'pencil', 'ruler', 'eras
